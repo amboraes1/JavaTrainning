3b70ffba9fed607bef8f73df449222c42af9e4f4
Tomas Alvarez <tomasalvarez@Taloss-Mac-mini.local>
2019-08-21 16:07:09 -0500

    Adding solution

--- java-training-feature-lesson6-source/src/main/java/com/talos/javatraining/lesson6/MainImpl.java
+++ java-training-feature-lesson6-source/src/main/java/com/talos/javatraining/lesson6/MainImpl.java
@@ -13,6 +13,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.StringUtils;
 import static java.util.Comparator.comparing;
 import static org.apache.commons.collections4.ComparatorUtils.chainedComparator;
@@ -26,179 +29,83 @@ public class MainImpl implements Main
 	@Override
 	public List<OrderData> getOrdersWithPriceGreaterThan(List<OrderData> orders, BigDecimal price, long limit)
 	{
-		List<OrderData> result = new ArrayList<>();
-		int i = 0;
-		for (OrderData order : orders)
-		{
-			if (order.getSubTotal().getValue().compareTo(price) > 0)
-			{
-				if (i++ < limit)
-				{
-					result.add(order);
-				}
-				else
-				{
-					break;
-				}
-			}
-		}
-		return result;
+		return orders
+				.stream()
+				.filter(x->x.getSubTotal().getValue().compareTo(price) > 0)
+				.limit(limit)
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public List<OrderData> getOrdersThatContainsAProduct(List<OrderData> orders, String productCode)
 	{
-		List<OrderData> result = new ArrayList<>();
-		for (OrderData order : orders)
-		{
-			boolean contains = false;
-			for (OrderEntryData entry : order.getEntries())
-			{
-				if (productCode.equals(entry.getProduct().getCode()))
-				{
-					contains = true;
-					break;
-				}
-			}
-			if (contains)
-			{
-				result.add(order);
-			}
-		}
-		return result;
+		return orders
+				.stream().filter(x->x.getEntries().stream().anyMatch(y->y.getProduct().getCode().equals(productCode)))
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public Set<String> getOrderCodes(List<OrderData> orders)
 	{
-		// In order to pass the test, you have to use a TreeSet
-		Set<String> result = new TreeSet<>();
-		for (OrderData order : orders)
-		{
-			result.add(order.getCode());
-		}
-		return result;
+		return orders.stream().map(OrderData::getCode).collect(Collectors.toCollection(TreeSet::new));
 	}
 
 	@Override
 	public List<OrderEntryData> getEntriesWithPriceLowerThan(List<OrderData> orders, BigDecimal price)
 	{
-		List<OrderEntryData> result = new ArrayList<>();
-		for (OrderData order : orders)
-		{
-			for (OrderEntryData entry : order.getEntries())
-			{
-				if (entry.getBasePrice().getValue().compareTo(price) < 0)
-				{
-					result.add(entry);
-				}
-			}
-		}
-		return result;
+		return orders
+				.stream()
+				.flatMap(x->x.getEntries().stream())
+				.filter(y->y.getBasePrice().getValue().compareTo(price) < 0)
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public Map<Integer, OrderEntryData> getEntriesAsMap(List<OrderData> orders, String orderCode)
 	{
-		OrderData selected = null;
-		for (OrderData order : orders)
-		{
-			if (order.getCode().equals(orderCode))
-			{
-				selected = order;
-				break;
-			}
-		}
-		if (selected == null)
-		{
-			return Collections.EMPTY_MAP;
-		}
-		Map<Integer, OrderEntryData> entries = new HashMap<>();
-		for (OrderEntryData entry : selected.getEntries())
-		{
-			entries.put(entry.getEntryNumber(), entry);
-		}
-		return entries;
+		return orders
+				.stream()
+				.filter(x->x.getCode().equals(orderCode))
+				.map(OrderData::getEntries)
+				.flatMap(List::stream)
+				.collect(Collectors.toMap(OrderEntryData::getEntryNumber,y->y));
 	}
 
 	@Override
 	public String getEntriesAsString(List<OrderData> orders, String orderCode)
 	{
-		OrderData selected = null;
-		for (OrderData order : orders)
-		{
-			if (order.getCode().equals(orderCode))
-			{
-				selected = order;
-				break;
-			}
-		}
-		if (selected == null)
-		{
-			return StringUtils.EMPTY;
-		}
-		StringBuilder builder = new StringBuilder();
-		for (OrderEntryData entry : selected.getEntries())
-		{
-			builder.append(PIPELINE).append(entry.toString());
-		}
-		String result = builder.toString();
-		return result.isEmpty() ? result : result.substring(1);
+		return orders
+				.stream()
+				.filter(x->x.getCode().equals(orderCode))
+				.map(OrderData::getEntries)
+				.flatMap(List::stream)
+				.map(OrderEntryData::toString)
+				.collect(Collectors.joining(PIPELINE));
 	}
 
 	@Override
 	public Map<String, List<ProductData>> getProductsByOrderCode(List<OrderData> orders)
 	{
-		Map<String, List<ProductData>> result = new HashMap<>();
-		for (OrderData order : orders)
-		{
-			for (OrderEntryData entry : order.getEntries())
-			{
-				result.compute(order.getCode(), (k, v) -> {
-					if (v == null)
-					{
-						v = new ArrayList<>();
-					}
-					v.add(entry.getProduct());
-					return v;
-				});
-			}
-		}
-		return result;
+		return orders
+				.stream()
+				.collect(Collectors.toMap(OrderData::getCode , x->x.getEntries().stream().map(OrderEntryData::getProduct).collect(Collectors.toList())));
 	}
 
 	@Override
 	public List<ProductData> getBestSellingProducts(List<OrderData> orders, long limit)
 	{
-		Map<ProductData, Long> counter = new HashMap<>();
-		for (OrderData order : orders)
-		{
-			for (OrderEntryData entry : order.getEntries())
-			{
-				counter.compute(entry.getProduct(), (k, v) -> {
-					if (v == null)
-					{
-						v = 0L;
-					}
-					return v + entry.getQuantity();
-				});
-			}
-		}
+		Map<ProductData, Long> counter = orders
+				.stream()
+				.map(OrderData::getEntries)
+				.flatMap(List::stream)
+				.collect(Collectors.toMap(OrderEntryData::getProduct,OrderEntryData::getQuantity,(Long::sum)));
+
 		Set<Map.Entry<ProductData, Long>> finalList = new TreeSet<>(COMPARATOR.reversed());
 		finalList.addAll(counter.entrySet());
-		List<ProductData> result = new ArrayList<>();
-		int i = 0;
-		for (Map.Entry<ProductData, Long> entry : finalList)
-		{
-			if (i++ < limit)
-			{
-				result.add(entry.getKey());
-			}
-			else
-			{
-				break;
-			}
-		}
-		return result;
+		return finalList
+				.stream()
+				.map(Map.Entry::getKey)
+				.limit(limit)
+				.collect(Collectors.toList());
 	}
 }
